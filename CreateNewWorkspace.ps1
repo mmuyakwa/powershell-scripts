70bc540efb16945025f2aaee39a226c81455a31c
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2017-12-24 15:32:00 +0100

    Translated the comments and added more to the ReadMe.

--- CreateNewWorkspace.ps1
+++ CreateNewWorkspace.ps1
@@ -1,23 +1,31 @@
+# This Script creates my daily folder for starting new projects.
+# Author: Michael Muyakwa, 2016-12-24
+# License: MIT
+#
 
-#Variable des Jahres-Ordner
+
+#Variable sets the foldername for the YEAR
 $Yfolder = "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))";
-#Variable des Monats-Ordner
+#Variable sets the foldername for the MONTH
 $Mfolder = "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))\$((Get-Date).ToString('MM'))";
 
-#Generiere Jahres-Ordner
+# If Folder with YEAR does not exist, create it
 if ( -Not (Test-Path $Yfolder) )
 {
     New-Item -ItemType Directory -Path $Yfolder;
 }
 
-#Generiere Monats-Ordner
+# If Folder with MOTH does not exist, create it
 if ( -Not (Test-Path $Mfolder) )
 {
     New-Item -ItemType Directory -Path $Mfolder;
 }
 
+# Has the full path (YEAR\MONTH) been created? If so, create the DAY-folder ("YYYY-MM-dd")
 if (Test-Path $Mfolder) 
 {
+    # CD into the created path
     Set-Location -Path "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))\$((Get-Date).ToString('MM'))";
+    # Create the folder "YYYY-MM-dd"
     New-Item -ItemType Directory -Path ".\$((Get-Date).ToString('yyyy-MM-dd'))";
 }
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# What this scripts do
+# I'm starting a little "script-colletion" here for *Powershell-Scripts*
 
-### CreateNewWorkspace.ps1
+# What these scripts do
+
+### -CreateNewWorkspace.ps1
 This creates a new folder in my folder 
-```php 
+```php
 'C:\Users\mmuya\Documents\Workspace\("CURRENT_YEAR")\("CURRENT_MONTH"))\("YYYY-MM-dd")'
 ```
+For my daily projects I start a new folder with the current Date.
+
+(e.g.: "c:\MyWorkspace\2017\12\2017-12-24")
