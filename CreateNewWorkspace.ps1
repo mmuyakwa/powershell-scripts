6528ddcdf23ec316982eb5a1cb8160871b14f4d5
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2017-12-26 00:36:10 +0100

    Added -openeing the folder in explorer

--- CreateNewWorkspace.ps1
+++ CreateNewWorkspace.ps1
@@ -8,24 +8,33 @@
 $Yfolder = "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))";
 #Variable sets the foldername for the MONTH
 $Mfolder = "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))\$((Get-Date).ToString('MM'))";
+#Variable sets the foldername for the Folder I want to create
+$Resultfolder = "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))\$((Get-Date).ToString('MM'))\$((Get-Date).ToString('yyyy-MM-dd'))";
 
 # If Folder with YEAR does not exist, create it
-if ( -Not (Test-Path $Yfolder) )
-{
+if ( -Not (Test-Path $Yfolder) ) {
     New-Item -ItemType Directory -Path $Yfolder;
 }
 
 # If Folder with MOTH does not exist, create it
-if ( -Not (Test-Path $Mfolder) )
-{
+if ( -Not (Test-Path $Mfolder) ) {
     New-Item -ItemType Directory -Path $Mfolder;
 }
 
 # Has the full path (YEAR\MONTH) been created? If so, create the DAY-folder ("YYYY-MM-dd")
-if (Test-Path $Mfolder) 
-{
-    # CD into the created path
-    Set-Location -Path "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))\$((Get-Date).ToString('MM'))";
-    # Create the folder "YYYY-MM-dd"
-    New-Item -ItemType Directory -Path ".\$((Get-Date).ToString('yyyy-MM-dd'))";
+if (Test-Path $Mfolder) {
+    # Checking if the foldername for the Folder I want to create already exists
+    if ( -Not (Test-Path $Resultfolder) ) {
+        # CD into the created path
+        Set-Location -Path "C:\Users\mmuya\Documents\Workspace\$((Get-Date).ToString('yyyy'))\$((Get-Date).ToString('MM'))";
+        # Create the folder "YYYY-MM-dd"
+        New-Item -ItemType Directory -Path ".\$((Get-Date).ToString('yyyy-MM-dd'))";
+        # Open the Folder in Explorer
+        $objShell = New-Object -ComObject "Shell.Application";
+        $objShell.Explore($Resultfolder);
+    } else {
+        # The Folder apparently already exists. Opening the Folder in Explorer
+        $objShell = New-Object -ComObject "Shell.Application";
+        $objShell.Explore($Resultfolder);
+    }
 }
