38454be9de796dccf4c28ac4476cdcc126aad39e
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2020-05-19 09:41:39 +0200

    Committed at: 2020-05-19 09:41 Uhr (automatic via script) [ci skip]

--- Install-DotNet_4.8.ps1
+++
@@ -1,5 +0,0 @@
-$scriptPath = Split-Path -Path $MyInvocation.MyCommand.Path -Parent
-# https://download.visualstudio.microsoft.com/download/pr/014120d7-d689-4305-befd-3cb711108212/0fd66638cde16859462a6243a4629a50/ndp48-x86-x64-allos-enu.exe
-$installer = "ndp48-x86-x64-allos-enu.exe"
-& "$($scriptPath)\$($installer)" /q /norestart
-
