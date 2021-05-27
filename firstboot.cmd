806e9032494ed4545accd18410d7e53741815a63
Michael Muyakwa <34812005+mmuyakwa@users.noreply.github.com>
2021-05-27 07:01:34 +0200

    Committed at: 2021-05-27 07:01 Uhr (automatic via script) [ci skip]

--- firstboot.cmd
+++ firstboot.cmd
@@ -1,4 +1,5 @@
 @echo off
 
+md "C:\image"
 Powershell.exe -Command "Invoke-WebRequest 'https://github.com/mmuyakwa/powershell-scripts/raw/master/FirstRun.ps1' -OutFile 'C:\image\FirstRun.ps1';"
 PowerShell.exe -ExecutionPolicy Unrestricted -Command "C:\image\FirstRun.ps1"
