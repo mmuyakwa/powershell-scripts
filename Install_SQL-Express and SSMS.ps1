b51111d58020c911f667bc1c3edf09ecf52da500
Michael M <34812005+mmuyakwa@users.noreply.github.com>
2021-07-05 14:17:53 +0200

    Add files via upload
    Install SQL-Express and SQL Management Studio

---
+++ Install_SQL-Express and SSMS.ps1
@@ -0,0 +1,5 @@
+﻿# Install SQL-Express and SQL Management Studio
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
+choco install sql-server-management-studio
+choco install sql-server-express
