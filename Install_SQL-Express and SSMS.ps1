3c87ba84778fd519bb0193622550b04036c67abf
Michael M <34812005+mmuyakwa@users.noreply.github.com>
2021-07-06 11:29:16 +0200

    Streamlined the code

--- Install_SQL-Express and SSMS.ps1
+++ Install_SQL-Express and SSMS.ps1
@@ -1,5 +1,4 @@
 ﻿# Install SQL-Express and SQL Management Studio
 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force
-choco install sql-server-management-studio
-choco install sql-server-express
+choco install sql-server-express sql-server-management-studio -y
