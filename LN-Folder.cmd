31d76e028145a04f37b3deaed634bdd43ce00fb4
Michael M <34812005+mmuyakwa@users.noreply.github.com>
2021-09-29 20:38:55 +0200

    Create Lasernet folders
    Best practice for initiating a folder after installation of Lasernet 8, 9 or 10

---
+++ LN-Folder.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+md C:\Lasernet\Archive
+md C:\Lasernet\File_Out
+md C:\Lasernet\Grab
+md C:\Lasernet\Log
+md C:\Lasernet\Preview
+md C:\Lasernet\RunTimeData
+md C:\Lasernet\XML_Archive
+md C:\Lasernet\XML_IN
+
